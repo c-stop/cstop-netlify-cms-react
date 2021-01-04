1b4c5ff8e985c5fdd7fdb39e26e4fc646f9ad4c1
Derek Etman <dereketman@gmail.com>
2021-01-04 10:04:24 -0800

    Chore: Cleared Console.log Feat: loader on form iframes

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import UseIframe from "./useIframe";
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import UseIframe from './useIframe'
 
 export default function Form({ data }) {
-	const [test, setTest] = useState();
-	// const {id, title} = data;
-	const { id } = useParams();
-	const form = data.find((p) => p.id === id);
+  const [test, setTest] = useState()
+  const { id } = useParams()
+  const form = data.find((p) => p.id === id)
 
-	// p.id === formId
-	useEffect(() => {
-		setTest(form);
-	}, [form]);
+  console.log(test)
 
-	// console.log("setting state in form: ", test)
-	let formData;
+  // p.id === formId
+  useEffect(() => {
+    setTest(form)
+  }, [form])
 
-	if (form) {
-		formData = <UseIframe {...test} />;
-	} else {
-		formData = <h2> Sorry. form doesn't exist </h2>;
-	}
+  // console.log("setting state in form: ", test)
+  let formData
 
-	return <div>{formData}</div>;
+  if (form) {
+    formData = <UseIframe {...test} />
+  } else {
+    formData = <h2> Sorry. form doesn't exist </h2>
+  }
+
+  return <div>{formData}</div>
 }
