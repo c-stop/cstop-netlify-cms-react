749d5bb00baf6ed381a4c456876e734e1cfbdf90
Derek Etman <dereketman@gmail.com>
2021-01-07 11:23:47 -0800

    seeing if adding style-loader will help

--- cms/webpack.config.js
+++ cms/webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           use: [
+            'style-loader',
             { loader: 'css-loader', options: { importLoaders: 1 } },
             'postcss-loader',
           ],
