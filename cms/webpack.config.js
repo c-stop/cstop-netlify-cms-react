edf39f785933daf0876da3af230be621ac23d805
Derek Etman <dereketman@gmail.com>
2021-01-07 12:06:19 -0800

    added file loader

--- cms/webpack.config.js
+++ cms/webpack.config.js
@@ -51,6 +51,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
